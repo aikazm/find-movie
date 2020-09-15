78f0aa66f8866c1eadb66283a6908e2f532c1df9
aikazm <zakiamelia@gmail.com>
2020-09-15 10:23:58 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';
+import './script/components/app-bar.js'
+
+document.addEventListener("DOMContentLoaded", main);
